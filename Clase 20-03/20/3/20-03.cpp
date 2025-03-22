921337d28d5c7ec4f2b8bbfe311eaed9ddcbe13c
Judi-fr <juanitoalcachofa55@hotmail.com>
2025-03-22 01:00:43 -0300

    finalmente complete el punto 2

--- Clase 20-03/20/3/20-03.cpp
+++ Clase 20-03/20/3/20-03.cpp
@@ -6,46 +6,50 @@ int main()
 {
     int matrix[30][12][31]={};
     int cargar_matriz(int matriz[30][12][31]);
+    void punto2(int matriz[30][12][31]);
 
     cargar_matriz(matrix);
+    punto2(matrix);
 
-    //PUNTO 2
+}
+void punto2(int matriz[30][12][31]){
     int m,d,e;
-    for(m=0;m<12;m++){
-        int contador=0;
-        for(d=0;d<=31;d++){
-            if(contador==30){cout<<"Mes: "<<m+1<<" Dia: "<<d<<endl;}
+    bool sin_asistencias=true;
+    for(m=1;m<=12;m++){
+        for(d=1;d<=31;d++){
+            sin_asistencias=true;
             for(e=0;e<30;e++){
-                if(matrix[e][m][d]==0){
-                    contador+=1;
+                if(matriz[e][m-1][d-1]>0){
+                    sin_asistencias=false;
                 }
+            }
+            if(sin_asistencias==true){
+                cout<<"Mes: "<<m<<" Dia: "<<d<<endl;
             }
         }
     }
-
 }
-
 int cargar_matriz(int matriz[30][12][31]){
    int empleado,mes,dia,horas;
-   cout<<"Ingresa el numero de empleado: ";
+   cout<<"Ingresa el numero de empleado excluyendo el 0: ";
    cin>>empleado;
-   do{
+   while(empleado !=0){
         cout<<"Ingresa el mes: ";
         cin>>mes;
+        mes-=1;
         cout<<"Ingresa el dia: ";
         cin>>dia;
+        dia-=1;
         cout<<"Ingresa las horas trabajadas: ";
         cin>>horas;
 
         matriz[empleado][mes][dia]=horas;
 
-        cout<<endl<<"si desea finalizar la carga ingrese"<<endl;
-        cout<<"dentro del numero de empleado un 0"<<endl;
+        cout<<endl<<"si desea finalizar la carga ingrese 0"<<endl;
         cout<<"------------------------------------"<<endl;
-        cout<<"si quiere continuar ingrese otro numero"<<endl<<endl;
         cout<<"ingresa el numero de empleado: ";
         cin>>empleado;
 
-   }while(empleado !=0);
+   };
     return matriz[30][12][31];
 }
